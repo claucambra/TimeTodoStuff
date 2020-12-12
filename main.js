289f45b7056b52304302103233d82c0726365d1b
elChupaCambra <claudio.cambra@gmail.com>
2020-12-12 15:50:46 +0100

    Added initial main.js and index.html files.

---
+++ main.js
@@ -0,0 +1,32 @@
+class TodoApp extends React.Component {
+	constructor (props) {
+		super (props);
+	}
+	render () {
+		return (
+			<TaskCreator />
+		);
+	}
+}
+
+class TaskCreator extends React.Component {
+	constructor (props) {
+		super (props);
+	}
+	render() {
+		return (
+			<input type="text" placeholder="New Task"></input>
+		);
+	}
+}
+
+class TasksView extends React.Component {
+	constructor (props) {
+		super (props);
+	}
+	render () {
+		return ();
+	}
+}
+
+ReactDOM.render(<TodoApp />, document.getElementbyId("container"));
