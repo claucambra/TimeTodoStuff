6cba9a309f391109e132962a0a0463debe60df89
elChupaCambra <claudio.cambra@gmail.com>
2020-12-13 12:12:03 +0100

    Fixing deleting of tasks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ class Task {
 	constructor(name, details) {
 		this.name = name;
 		this.details = details;
-		this.time = Date.now();
+		this.id = Date.now();
 		this.status = 0;
 	}
 }
@@ -23,7 +23,7 @@ class TodoApp extends React.Component {
 	}
 	
 	deleteTask(id) {
-		this.setState(state => state.tasks.filter(task => task.time != id));
+		this.setState(state => state.tasks = state.tasks.filter(task => task.id != id));
 	}
 	
 	render () {
@@ -58,9 +58,9 @@ class TaskCreator extends React.Component {
   
 	render() {
 		return (
-			<div class="input-group">
-				<input type="text" id="taskName" class="form-control" placeholder="New Task" onChange={this.inputHandler}></input>
-				<button type="button" class="btn btn-outline-secondary" onClick={this.submitHandler}>Submit</button>
+			<div className="input-group">
+				<input type="text" id="taskName" className="form-control" placeholder="New Task" onChange={this.inputHandler}></input>
+				<button type="button" className="btn btn-outline-secondary" onClick={this.submitHandler}>Submit</button>
 			</div>
 		);
 	}
@@ -73,17 +73,17 @@ class TasksView extends React.Component {
 	}
 	
 	deleteHandler(event) {
-		this.props.deleter(event.target.id);
+		this.props.deleter(event.target.parentElement.id);
 	}
 	
 	render () {
 		var taskCards = this.props.tasks.map(task => {
-			return(<div class="taskCard" id={task.time}>
+			return(<div class="taskCard" id={task.id}>
 				<h1>{task.name}</h1>
 				<p>{task.details}</p>
-				<button type="button" class="btn btn-success">Completed</button>
-				<button type="button" class="btn btn-primary">More details</button>
-				<button type="button" class="btn btn-danger" onClick={this.deleteHandler}>Delete</button>
+				<button type="button" className="btn btn-success">Completed</button>
+				<button type="button" className="btn btn-primary">More details</button>
+				<button type="button" className="btn btn-danger" onClick={this.deleteHandler}>Delete</button>
 			</div>)
 		});
 		console.log(this.props.tasks);
