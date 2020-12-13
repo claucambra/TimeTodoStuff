e3cb99ffe427633b35d9eadbc234c33be643b35d
elChupaCambra <claudio.cambra@gmail.com>
2020-12-13 11:14:09 +0100

    Testing bootstrap components

--- main.js
+++ main.js
@@ -46,10 +46,10 @@ class TaskCreator extends React.Component {
 		this.setState({name: event.target.value});
 	}
 	
-  submitHandler() {
-    console.log("added")
-    this.props.adder(this.state.name, "placeholder detail");
-  }
+	submitHandler() {
+		console.log("added")
+		this.props.adder(this.state.name, "placeholder detail");
+	}
   
 	render() {
 		return (
@@ -67,7 +67,15 @@ class TasksView extends React.Component {
 	}
 	
 	render () {
-		var taskCards = this.props.tasks.map(task => <div class="taskCard"><h1>{task.name}</h1><p>{task.details}</p></div>);
+		var taskCards = this.props.tasks.map(task => {
+			return(<div class="taskCard">
+				<h1>{task.name}</h1>
+				<p>{task.details}</p>
+				<button type="button" class="btn btn-success">Completed</button>
+				<button type="button" class="btn btn-primary">More details</button>
+				<button type="button" class="btn btn-danger">Delete</button>
+			</div>)
+		});
 		console.log(this.props.tasks);
 		return (<div>{taskCards}</div>);
 	}
